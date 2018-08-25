c16c10e9c542dac493509c8566bcf82344457128
ukn <>
2018-08-25 21:10:37 +1000

    added mirror sound
    should we wait for sound to end to play again?

---
+++ Maker August 2018/Assets/Scripts/PlayerSoundTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSoundTrigger : MonoBehaviour {
+
+    public AudioClip[] clips;
+
+    AudioSource audioSource;
+
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "mirror" && !audioSource.isPlaying)
+        {
+            int randomInt = Random.Range(0, 8);
+            audioSource.clip = clips[randomInt];
+            audioSource.Play();
+        }
+    }
+
+}
