17c4ba79792733fcddaff622c42d9fae6907a00e
ukn <>
2018-08-25 21:35:47 +1000

    changed mirror sound
    each mirror now has own audio source

--- Maker August 2018/Assets/Scripts/PlayerSoundTrigger.cs
+++ Maker August 2018/Assets/Scripts/PlayerSoundTrigger.cs
@@ -14,11 +14,22 @@ public class PlayerSoundTrigger : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.gameObject.tag == "mirror" && !audioSource.isPlaying)
+        /*if(other.tag == "mirror" && !audioSource.isPlaying)
         {
             int randomInt = Random.Range(0, 8);
             audioSource.clip = clips[randomInt];
             audioSource.Play();
+        }*/
+
+        if (other.tag == "mirror")
+        {
+            AudioSource mirrorAudio;
+            mirrorAudio = other.gameObject.GetComponent<AudioSource>();
+
+            if(!mirrorAudio.isPlaying)
+            {
+                mirrorAudio.Play();
+            }
         }
     }
 
