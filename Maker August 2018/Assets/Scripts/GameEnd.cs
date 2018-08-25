4adef01d691b14450350dd034b36209f856b4a77
ukn <>
2018-08-25 18:35:17 +1000

    init

---
+++ Maker August 2018/Assets/Scripts/GameEnd.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEnd : MonoBehaviour
+{
+    public float endDelay = 5f;
+    public GameObject mainCamera;
+    public GameObject endScreen;
+    private int gameSuccess = 0;
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && gameSuccess == 0)
+        {
+            gameSuccess = 1;
+            Debug.Log("HIT");
+            Invoke("EndGame", endDelay);
+            Debug.Log("Complete");
+        }
+    }
+
+    void EndGame()
+    {
+        Debug.Log("Moving Camera");
+        SceneManager.LoadScene("EndScene");
+
+
+    }
+
+
+
+}
---
+++ Maker August 2018/Assets/Scripts/MovePlatformUp.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MovePlatformUp : MonoBehaviour {
+
+    public float moveForce = 5f;
+    public GameObject platform;
+    public Transform destinationPoint;
+    private bool moving = false;
+   
+    // Use this for initialization
+    void Update () {
+        if (moving)
+        {
+            platform.transform.position = Vector3.MoveTowards(platform.transform.position, destinationPoint.position, Time.deltaTime * moveForce);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            moving = true;
+
+        }
+    }
+
+    void Rise()
+    {
+        
+    }
+}
