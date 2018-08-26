a036fa01b8cf5942ba8487e8a32981d0a2f27e24
ukn <>
2018-08-26 11:07:33 +1000

    sprite switches at rightmost mirror

---
+++ Maker August 2018/Assets/Scripts/SpriteChanger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteChanger : MonoBehaviour {
+
+    public Sprite spriteA;
+    public Sprite spriteB;
+
+    bool changed = false;
+    SpriteRenderer spriteRenderer;
+
+	void Start () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "changer")
+        {
+            Debug.Log("hello");
+
+            if (!changed)
+            {
+                spriteRenderer.sprite = spriteB;
+                changed = !changed;
+            } else
+            {
+                spriteRenderer.sprite = spriteA;
+                changed = !changed;
+            }
+        }
+
+    }
+}
